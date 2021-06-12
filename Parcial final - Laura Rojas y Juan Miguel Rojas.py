97a249f972c9644d13741068b71f954b9703c9af
JuanRx19 <63820244+JuanRx19@users.noreply.github.com>
2021-06-12 14:13:47 -0500

    Add files via upload

--- Parcial final - Laura Rojas y Juan Miguel Rojas.py
+++ Parcial final - Laura Rojas y Juan Miguel Rojas.py
@@ -1,19 +1,40 @@
 #Procedimiento el cual me permite imprimir el valor final y los productos
 def total(id, rol, produc):
-    total = 0
-    print("La persona cedula " + str(id) + " y rol de " + str(rol) + " compro los siguientes productos: ")
-    for elem in produc:
-        print("Codigo: " + str(elem))
-        print("Nombre: " + str(produc[elem][0]))
-        print("Cantidad: " + str(produc[elem][1]) + " unidades\n")
-        total += produc[elem][1] * produc[elem][2]
-    print("Total a pagar: " + str(total))
+    totalParcial = 0
+    if rol == "Estudiante" or rol == "Profesor":
+        print("El " + str(rol) + " con cedula " + str(id) + ", por la compra de los productos: ")
+        for elem in produc:
+            print("Codigo: " + str(elem))
+            print("Nombre: " + str(produc[elem][0]))
+            print("Cantidad: " + str(produc[elem][1]) + " unidades")
+            print()
+            totalParcial += produc[elem][1] * produc[elem][2]
+            if rol == "Profesor":
+                total = totalParcial - ((totalParcial * 20)/100)
+            elif rol == "Estudiante":
+                total = (totalParcial * 50)/100
+        print("Debe pagar un total de " + str(total))
 
 #Diccionario creado inicialmente
 productosavender = {}
 comprar = 1
 id = int(input("Por favor digite su cedula: "))
-rol = input("Por favor digite su rol (Profesor o Estudiante): ")
+p = True
+while(p == True):
+    try:
+        rol = int(input("Por favor digite su rol 1. Profesor o 2. Estudiante: "))
+        if(rol == 1):
+            rol = "Profesor"
+            p = False
+        elif(rol == 2):
+            rol = "Estudiante"
+            p = False
+        else:
+            print("Por favor digite una opción correcta.")
+    except:
+        print("Por favor digite una opción correcta.")
+
+
 #Ciclo el cual me permite repetir la cantidad de veces que un usuario quiera registrar distintos productos
 while (comprar != 2):
     producto = int(input("Por favor digite el codigo del producto: "))
@@ -22,6 +43,7 @@ while (comprar != 2):
     precio = eval(input("Por favor digite el precio del producto: "))
     productosavender[producto] = [nombre, unidades, precio]
     comprar = int(input("¿Desea ingresar otro producto? 1. Si 2. No: "))
+        
 
 #Llamado del procedimiento total, que me permite hacer la impresión
 total(id, rol, productosavender)
